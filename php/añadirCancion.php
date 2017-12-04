3244cd1bbdceb26844025d16afbc74dfe3c0dad3
Gorka <gorkaar@hotmail.com>
2017-12-04 13:36:56 +0100

    añadido subir archivo de audio

--- php/añadirCancion.php
+++ php/añadirCancion.php
@@ -1,29 +1,54 @@
 <?php
 	if (isset($_POST['titulo'])){
-				if(empty($_POST['artista'])|| empty($_POST['genero'])|| empty($_POST['ano'])){
+				if(empty($_POST['artista'])|| empty($_POST['genero'])|| empty($_POST['ano'] ||empty($_FILES["audio"])){
 					echo "Rellene todos los campos.";
 				}else{
-					$xml = simplexml_load_file('canciones.xml');
+              if (is_uploaded_file($_FILES["audio"]["tmp_name"])){
+                      $ruta = "../Users/'.$_SESSION["email"].'/".$_FILES["audio"]["name"];
+                      move_uploaded_file($_FILES["imagen"]["tmp_name"],$ruta);
+              }
 
-					$cancion = $xml->addChild('cancion');
+    					$xml = simplexml_load_file('../Users/'.$_SESSION["email"].'/canciones.xml');
 
-					$titulo = $cancion -> addChild('titulo');
-					$titulo -> addChild('value',$_POST['titulo']);
+    					$cancion = $xml->addChild('cancion');
 
-          $artista = $cancion -> addChild('artista');
-					$artista -> addChild('value',$_POST['artista']);
+    					$titulo = $cancion -> addChild('titulo');
+    					$titulo -> addChild('value',$_POST['titulo']);
 
-          $genero = $cancion -> addChild('genero');
-					$genero -> addChild('value',$_POST['genero']);
+              $artista = $cancion -> addChild('artista');
+    					$artista -> addChild('value',$_POST['artista']);
 
-          $ano = $cancion -> addChild('ano');
-					$ano -> addChild('value',$_POST['ano']);
+              $genero = $cancion -> addChild('genero');
+    					$genero -> addChild('value',$_POST['genero']);
 
-          $path = $Cancion -> addChild('path');
-					$path -> addChild('value',$_POST['path']);
+              $ano = $cancion -> addChild('ano');
+    					$ano -> addChild('value',$_POST['ano']);
 
-					$xml->asXML('canciones.xml');
+              $path = $cancion -> addChild('path');
+    					$path -> addChild('value','../Users/'.$_SESSION["email"].'/'.$_FILES["audio"]["name"]);
 
+    					$xml->asXML('canciones.xml');
+
+              $xml2 = simplexml_load_file('../canciones.xml');
+
+    					$cancion = $xml2->addChild('cancion');
+
+    					$titulo = $cancion -> addChild('titulo');
+    					$titulo -> addChild('value',$_POST['titulo']);
+
+              $artista = $cancion -> addChild('artista');
+    					$artista -> addChild('value',$_POST['artista']);
+
+              $genero = $cancion -> addChild('genero');
+    					$genero -> addChild('value',$_POST['genero']);
+
+              $ano = $cancion -> addChild('ano');
+    					$ano -> addChild('value',$_POST['ano']);
+
+              $path = $cancion -> addChild('path');
+    					$path -> addChild('value','../Users/'.$_SESSION["email"].'/'.$_FILES["audio"]["name"]);
+
+    					$xml2->asXML('canciones.xml');
 				}
 			}
 ?>
