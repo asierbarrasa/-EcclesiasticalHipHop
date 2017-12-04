4ee078a6f4ae98157d5a312d188eef6ce1842df0
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-12-04 18:18:59 +0100

    ..

--- php/getListaCanciones.php
+++ php/getListaCanciones.php
@@ -6,7 +6,7 @@ echo '<table border=1> <tr> <th> Nombre </th> <th> Artista </th>'
 foreach($xml->xpath('//cancion') as $cancion){
 
     echo '<tr><td>'.$cancion->nombre->value[0].'</td><td>'.$cancion->artista->value[0].'</td><td>'.$cancion->genero->value[0].'</td><td>'
-    .'<audio controls><source src="'$cancion->path->value[0]'" type="audio/mpeg"></audio></td></tr>';
+    .'<audio controls><source src="'.$cancion->path->value[0].'" type="audio/mpeg"></audio></td></tr>';
 }
 
 
