79566816276252bbc7c54d9752557ec4dc7b23dd
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-12-04 18:30:15 +0100

    Mejorando el panorama

--- php/getListaCanciones.php
+++ php/getListaCanciones.php
@@ -1,11 +1,11 @@
 <?php
 $xml = simplexml_load_file("../xml/canciones.xml");
 echo '<table border=1> <tr> <th> Nombre </th> <th> Artista </th>'
-    .'<th> Genero </th><th> Reproducir </th></tr>';
+    .'<th> Genero </th><th> Año </th><th> Reproducir </th></tr>';
 
 foreach($xml->xpath('//cancion') as $cancion){
 
-    echo '<tr><td>'.$cancion->nombre->value[0].'</td><td>'.$cancion->artista->value[0].'</td><td>'.$cancion->genero->value[0].'</td><td>'
+    echo '<tr><td>'.$cancion->nombre->value[0].'</td><td>'.$cancion->artista->value[0].'</td><td>'.$cancion->genero->value[0].'</td><td>'.$cancion->ano->value[0].'</td><td>'
     .'<audio controls><source src="'.$cancion->path->value[0].'" type="audio/mpeg"></audio></td></tr>';
 }
 
