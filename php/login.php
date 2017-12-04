073394247048d21a80108986eb8975b4f68cd733
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-12-04 13:21:51 +0100

    login.php

---
+++ php/login.php
@@ -0,0 +1,19 @@
+<?php
+
+$email = $_POST["email"];
+$pass = crypt($_POST["pass"], "Ap");
+$link = mysqli_connect("localhost", "id2921858_ecclHipHop","gruposar6","id2921858_ecclHipHop");
+$sql = "Select * from Users where email = $email";
+$result = mysqli_query($link, $sql);
+$row = mysqli_fetch_assoc($result);
+if(mysqli_num_rows ($result)==1){
+  if($row["pass"]==$pass){
+    session_start();
+    $_SESSION["email"] = $email;
+    echo "Correcto";
+  }
+  else{
+    die("Error");
+  }
+
+?>
