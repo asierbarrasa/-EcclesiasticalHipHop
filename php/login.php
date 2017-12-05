d46a909b14fa3dae181f601511132eb3e461f181
asierbarrasa <asierjarrillero@gmail.com>
2017-12-05 13:08:54 +0100

    login

--- php/login.php
+++ php/login.php
@@ -1,19 +1,21 @@
 <?php
-
 $email = $_POST["email"];
-$pass = crypt($_POST["pass"], "Ap");
+$pass =$_POST["pass"];
+echo $email;
+echo $pass;
 $link = mysqli_connect("localhost", "id3865054_ecclhiphop","******","id3865054_ecclhiphop");
 $sql = "Select * from Users where email = $email";
 $result = mysqli_query($link, $sql);
 $row = mysqli_fetch_assoc($result);
 if(mysqli_num_rows ($result)==1){
-  if($row["pass"]==$pass){
+  if($row["pass"]==(crypt($pass,$row["pass"]))){
     session_start();
     $_SESSION["email"] = $email;
     echo "Correcto";
   }
   else{
-    die("Error");
+      echo "Hola holita vecinito";
+   // die("Error");
   }
-
+}
 ?>
