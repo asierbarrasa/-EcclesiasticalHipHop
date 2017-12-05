79a8fb7e2ebbbc11cac69d9c02ae2a031d5887f8
Aitor Diaz de Otazu <aitor@MacBook-Pro-de-Aitor.local>
2017-12-05 12:26:34 +0100

    Creada lista de canciones propias del usuario

---
+++ php/getBiblioteca.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$us = $_SESSION["email"];
+$path = "../Users/$us/canciones.xml";
+$xml = simplexml_load_file($path);
+echo '<table border=1> <tr> <th> Nombre </th> <th> Artista </th>'
+    .'<th> Genero </th><th> Año </th><th> Reproducir </th></tr>';
+
+foreach($xml->xpath('//cancion') as $cancion){
+
+    echo '<tr><td>'.$cancion->nombre->value[0].'</td><td>'.$cancion->artista->value[0].'</td><td>'.$cancion->genero->value[0].'</td><td>'.$cancion->ano->value[0].'</td><td>'
+    .'<audio controls><source src="'.$cancion->path->value[0].'" type="audio/mpeg"></audio></td></tr>';
+}
+
+
+echo '</table>';
+
+echo ' <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>';
+echo  '<script>$("audio").bind("play", function() {
+  activated = this;
+  $('.'"audio"'.').each(function() {
+    if(this != activated) this.pause();
+  });
+});</script>';
+
+
+ ?>
